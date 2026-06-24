87969201c69501dbaa29d14aa3db183a79913011
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Glue/CompanyUserAuthRestApi/CompanyUserAuthRestApiConfig.php
+++ src/Spryker/Glue/CompanyUserAuthRestApi/CompanyUserAuthRestApiConfig.php
@@ -12,46 +12,64 @@ use Spryker\Glue\Kernel\AbstractBundleConfig;
 class CompanyUserAuthRestApiConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_COMPANY_USER_ACCESS_TOKENS = 'company-user-access-tokens';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTROLLER_COMPANY_USER_ACCESS_TOKENS_RESOURCE = 'company-user-access-tokens-resource';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CLIENT_GRANT_USER = 'idCompanyUser';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_MISSING_ACCESS_TOKEN = 'Missing access token.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_INVALID_ACCESS_TOKEN = 'Invalid access token.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_INVALID_LOGIN = 'Failed to authenticate user.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_ACCESS_CODE_INVALID = '001';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_FORBIDDEN = '002';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_INVALID_LOGIN = '003';
